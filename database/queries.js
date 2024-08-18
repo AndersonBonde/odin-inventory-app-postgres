6e1fdd51562c5e6c6f7827ca2756547d8d36ce31
Anderson <andersonbonde@gmail.com>
2024-08-18 10:52:54 -0300

    Add scaffolding to database folder

---
+++ database/queries.js
@@ -0,0 +1,15 @@
+const pool = require('./pool');
+
+async function getAllCategories() {
+  console.log('Get all categories WIP');
+};
+
+async function getAllItems() {
+  console.log('Get all items WIP');
+};
+
+module.exports = {
+  getAllCategories,
+  getAllItems,
+
+}
